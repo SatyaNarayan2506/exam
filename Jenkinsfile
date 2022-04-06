09e2e033397eefea058d0bfb1bc61920c893660b
SatyaNarayan2506 <snsaini256@gmai.com>
2022-04-06 10:31:06 +0530

    initial

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,24 +1,27 @@
-pipeline {
-    agent any 
-    stages {
-        stage ('git clone') {
-            steps {
-                checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/SatyaNarayan2506/exam.git']]])
+pipeline{
+    agent any
+    stages{
+        stage('Git checkout'){
+            steps{
+                checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'joy-git', url: 'https://github.com/SatyaNarayan2506/exam.git']]])
             }
         }
-        stage ('terafform init') {
-            steps {
-                sh '''
-                terraform init
-                terraform plan 
-                '''
+        stage('Inite'){
+            steps{
+                sh 'terraform init'
             }
-
-        }        
-        stage ('terraform apply in gcp') {
-            steps {
-                sh 'terraform destroy -auto-approve'
+        }
+        stage('Plan'){
+            steps{
+                sh 'terraform plan -var-file=terraform.tfvars'
             }
-        } 
+        }
+        stage('Apply'){
+            steps{
+                //sh 'terraform apply -var-file=terraform.tfvars -auto-approve'
+                sh 'terraform destroy -var-file=terraform.tfvars -auto-approve'
+            }
+        }
+        
     }
 }
--- main.tf
+++ main.tf
@@ -1,29 +1,63 @@
+// Configure the Google Cloud provider
 provider "google" {
-  project = "cellular-cider-339905"
-  region  = "us-central1"
-  zone    = "us-central1-c"
-  credentials = "satya.json"
+ credentials = "${file("${var.credentials}")}"
+ project     = "${var.gcp_project}"
+ region      = "${var.region}"
+ zone        = "${var.zone}"
+}
+resource "google_compute_network" "vpc_network" {
+name = "${var.vpc-name}"
+}
+resource "google_compute_subnetwork" "public-subnetwork" {
+name = "${var.subnet-name}"
+ip_cidr_range = "${var.ip_cidr_range}"
+region = "${var.region}"
+network = google_compute_network.vpc_network.name
+}
+// VPC firewall configuration
+resource "google_compute_firewall" "demo-firewall" {
+  name    = "${var.firewall-name}"
+  network = "${google_compute_network.vpc_network.name}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "8080", "1000-2000"]
+    
+  }
+
+  source_ranges = ["0.0.0.0/0"]
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "f1-micro"
+resource "google_compute_instance" "vm-instance" {
+  name         = "${var.instance_name}"
+  machine_type = "${var.machine_type}"
+  zone         = "${var.zone}"
+
+  tags = ["foo", "bar"]
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-2004-focal-v20220331"
+      image = "${var.image}"
     }
   }
+
 
   network_interface {
-    # A default network is created for all GCP projects
-    network = google_compute_network.vpc_network.self_link
+    network = "${var.network}"
+
     access_config {
+      // Ephemeral public IP
     }
   }
-}
 
-resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
-  auto_create_subnetworks = "true"
+  metadata = {
+    foo = "bar"
+  }
+  
+
+ 
 }
